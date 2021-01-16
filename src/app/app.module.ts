56ecc7719c47d17f4514d1e2fdeb0605190f5f0e
shrirajthorat <shrirajthorat33@gmail.com>
2021-01-16 22:34:12 +0530

    Shree 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-<<<<<<< HEAD
 import { CarCategoryComponent } from './car-category/car-category.component';
 import { HttpClientModule } from "@angular/common/http";
-import { ReactiveFormsModule,FormsModule } from "@angular/forms";
-=======
+
 import { MembershipRegistrationComponent } from './membership-registration/membership-registration.component';
 import { MemberLoginComponent } from './member-login/member-login.component';
-import {HttpClientModule} from '@angular/common/http';
+
 import { CustomerService } from './customer.service';
 import { CustomerAdapter } from './customer';
 import { BookingDetailsComponent } from './booking-details/booking-details.component';
@@ -34,7 +32,6 @@ import { CarouselModule, WavesModule } from 'angular-bootstrap-md';
 import { StaffloginComponent } from './stafflogin/stafflogin.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { MatCardModule,} from '@angular/material/card';
->>>>>>> d8a147d0c7e07fd56bf2ea29e4a58ea469bcd33b
 
 @NgModule({
   declarations: [
@@ -42,9 +39,7 @@ import { MatCardModule,} from '@angular/material/card';
     HeaderComponent,
     NavbarComponent,
     FooterComponent,
-<<<<<<< HEAD
     CarCategoryComponent,
-=======
     MembershipRegistrationComponent,
     MemberLoginComponent,
     BookingDetailsComponent,
@@ -54,21 +49,15 @@ import { MatCardModule,} from '@angular/material/card';
     CarouselComponent,
     StaffloginComponent,
     PagenotfoundComponent
->>>>>>> d8a147d0c7e07fd56bf2ea29e4a58ea469bcd33b
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     MDBBootstrapModule.forRoot(),
-<<<<<<< HEAD
     HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
-=======
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule,
     BrowserAnimationsModule,
     MatGridListModule,
     MatDatepickerModule,
@@ -80,7 +69,6 @@ import { MatCardModule,} from '@angular/material/card';
     MatCheckboxModule,
     CarouselModule, WavesModule,
     MatCardModule,
->>>>>>> d8a147d0c7e07fd56bf2ea29e4a58ea469bcd33b
   ],
   providers: [CustomerService,CustomerAdapter],
   bootstrap: [AppComponent]
